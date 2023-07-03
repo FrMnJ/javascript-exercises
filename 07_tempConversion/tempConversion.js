57927ea3a9da254c2fbf8d42a48d49c803538a59
FrMnJ <manuelserratossec38@gmail.com>
2023-07-03 12:16:01 -0600

    Add last solutions for fundamentals 4

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let res=(fahrenheit-32)*5/9;
+  res=Math.round(res*10)/10;
+  return res;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let res=9/5*celsius+32;
+  res=Math.round(res*10)/10;
+  return res;
 };
 
 // Do not edit below this line
