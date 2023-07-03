b71441ba1ef94565a746b92f6031ccd2e3694667
FrMnJ <manuelserratossec38@gmail.com>
2023-07-03 11:43:18 -0600

    ADd removeFormArray solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr,...args) {
+    const res=[]
+    for(let i=0;i<arr.length;i++){
+        let toNewArray=true;
+        for(let j=0;j<arguments.length;j++)
+            if(arr[i]===arguments[j]) toNewArray=false;
+        if(toNewArray) res.push(arr[i]);
+    }
+    return res
 };
 
 // Do not edit below this line
