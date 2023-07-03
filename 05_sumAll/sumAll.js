2ea38746a072c8b60f51f34fa9ada59230e92b44
FrMnJ <manuelserratossec38@gmail.com>
2023-07-03 11:56:33 -0600

    Add sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(from,to ) {
+    if(typeof from !== 'number'||typeof to !=='number') return "ERROR";
+    if(from<0||to<0) return "ERROR";
+    let res=0;
+    let min=Math.min(from,to);
+    let max=Math.max(from,to);
+    from=min;
+    to=max;
+    for(let i=from;i<=to;i++) res+=i;
+    return res;
 };
 
 // Do not edit below this line
