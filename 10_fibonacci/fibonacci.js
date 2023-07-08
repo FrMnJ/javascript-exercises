8e2ca1bca117694007546466e29f6f3dc17471c8
FrMnJ <manuelserratossec38@gmail.com>
2023-07-07 19:33:05 -0600

    10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+let fib=[1,1];
+function fibon(num){
+    if(fib[num]!==undefined) return fib[num];
+    else
+        fib[num]=fibon(num-1)+fib[num-2];
+    return fib[num];
+}
 
+const fibonacci = function(num) {
+    if(num<0) return "OOPS";
+    return fibon(parseInt(num)-1);
 };
 
 // Do not edit below this line
