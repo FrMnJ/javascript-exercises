4b9d3f047a01a484617e1181fcbc264b1666d078
FrMnJ <manuelserratossec38@gmail.com>
2023-07-07 19:23:09 -0600

    Add 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let res="";
+    for(let i=0;i<str.length;i++){
+        if((str[i].toLowerCase()>='a'&&str[i].toLowerCase()<='z')||(str[i]>='0'&&str[i]<='9')) res+=str[i].toLowerCase();
+    }
+    const reversedStr=res.split("").reverse().join("");
+    if(reversedStr===res) return true;
+    else return false;
+};  
 
-};
 
 // Do not edit below this line
 module.exports = palindromes;
