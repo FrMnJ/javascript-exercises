6abad46a39d33d512f937f4f51e90709a232b6cb
FrMnJ <manuelserratossec38@gmail.com>
2023-07-07 19:46:01 -0600

    Add 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort((a,b)=>{
+        if(a.yearOfDeath===undefined) a.yearOfDeath=new Date().getFullYear();
+        if(b.yearOfDeath===undefined) b.yearOfDeath=new Date().getFullYear();
+        if((a.yearOfDeath-a.yearOfBirth)>(b.yearOfDeath-b.yearOfBirth)) return -1;
+        return 1;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
